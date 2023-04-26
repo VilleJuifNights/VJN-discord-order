841c664de1aa3fa03a51f228f7fdab4b65ee8351
clovislfbv <clovis.lefebvre.london@gmail.com>
2023-04-26 16:29:25 +0200

    adapt the code to the discord.py module

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This bot allows you to order crepes and other food during Villejuif nights via Discord
 
+## How does the bot currently works ?
+
+Currently, the bot works with slash commands but our goal is to make the bot working later with only buttons or reactions to make it easier to use.
+
 ## How to contribute to the bot ?
 
 ### Cloning the repo
@@ -9,7 +13,9 @@ This bot allows you to order crepes and other food during Villejuif nights via D
 git clone git@github.com:VilleJuifNights/VJN-discord-order.git
 ```
 
-### Installing the right packages
-For this bot I used this module: https://github.com/mccoderpy/discord.py-message-components
+### Requirements
+To run the bot you need to have the discord.py module installed on your computer.
+If you didn't installed it, checkout this link : https://github.com/Rapptz/discord.py/tree/master
 
-This module allows you to have all the functionalities of discord.py (slash commands, buttons...) in a single module. Please follow the steps indicated in the README of the module to install it
+Moreover, you need to add the id of the channel where you want the commands will be either in a file called "commands_channel.txt" in the same directory or either directly in the code.
+Obviously, you also need to add your own bot token either in a file called "token_bot.txt" or either directly in the code.
--- vjn_bot.py
+++ vjn_bot.py
@@ -1,79 +1,80 @@
 import discord
-from discord.ext import commands, tasks
-from discord import ApplicationCommandInteraction as APPCI, SlashCommandOptionChoice as Choice, SlashCommandOption as Option, Button, ButtonStyle
+from discord import app_commands, Game, Activity, ActivityType
 from datetime import datetime
-from random import choice
 
-bot = commands.Bot(command_prefix="$", sync_commands = True)
-client = discord.Client()
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
-@bot.event
+@client.event
 async def on_ready():
     '''
     show if there is no error and start the RPC
     '''
     print("ready")
-    changeStatus.start()
+    game = Game(name="/commander")
+    activity = Activity(name="Playing", type=ActivityType.playing)
+    await client.change_presence(activity=game)
 
-status = ["/commander"]
 commandes = {}
 identifiant = 1
 
-@bot.command()
-async def start(ctx, secondes = 5):
-    '''
-    function to display messages stored in the list called status
-    '''
-    changeStatus.change_interval(seconds = secondes)
-
-@tasks.loop(seconds = 5)
-async def changeStatus():
-    '''
-    if there are several messages to display, it will make it change every 5 seconds
-    '''
-    game = discord.Game(choice(status))
-    await bot.change_presence(activity = game)
-
-@bot.slash_command(name="commander", description = "pour commander des trucs bons", options=[
-    Option(name="choix", description="le choix que tu veux faire", option_type=3, required=True, choices=[Choice(name = "Crèpe usuelle", value = "crèpe usuelle"), Choice(name = "Crèpe combinaison", value = "crèpe combinaison"), Choice(name = "Churros", value = "churros"), Choice(name = "Gauffres", value = "gauffres"), Choice(name = "Barbe à papa", value = "barbes à papa"), Choice(name = "soda", value = "soda")])
-])
-async def commander(ctx:APPCI, choix):
+@tree.command(name="commander")
+@app_commands.describe(choix="pour commander des trucs bons")
+@app_commands.choices(choix=[
+    app_commands.Choice(name = "Crèpe usuelle", value = "crèpe usuelle"), 
+    app_commands.Choice(name = "Crèpe combinaison", value = "crèpe combinaison"), 
+    app_commands.Choice(name = "Churros", value = "churros"), 
+    app_commands.Choice(name = "Gauffres", value = "gauffres"), 
+    app_commands.Choice(name = "Barbe à papa", value = "barbes à papa"), 
+    app_commands.Choice(name = "soda", value = "soda")
+    ])
+async def commander(interaction: discord.Interaction, choix: app_commands.Choice[str]):
     '''
     to order food
     '''
     global identifiant
-    channel = bot.get_channel(1094212525157007410)
-    people = await ctx.author.create_dm()
+    choix = choix.value
+    with open("commands_channel.txt", "r") as commands_channel:
+        channel = client.get_channel(int(commands_channel.read()))
+    people = await interaction.user.create_dm()
     link = ""
     if choix == "soda":
         link = "https://boutique.sofratel.fr/laon/wp-content/uploads/sites/17/2021/12/boissons33cl.jpg"
     title = "Commande n°" + str(identifiant) + "\ncommandé à " + datetime.now().strftime("%H:%M")
     commandes[identifiant] = (choix, people, title)
-    emb = discord.Embed(title=title, description=f"{ctx.author.mention} a commandé des {choix}", color=0x3498db)
+    emb = discord.Embed(title=title, description=f"{interaction.user.mention} a commandé des {choix}", color=0x3498db)
     message = discord.Embed(title=title, description=f"Tu a commandé : {choix}", color=0x3498db)
     if (link != ""):
         emb.set_image(url=link)
     await channel.send(embed=emb)
     await people.send(embed=message)
     await people.send("Il faut maintenant que tu viennes au stand pour pouvoir valider et préparer ta commande")
-    await ctx.respond("Ta commande a bien été enregistré")
+    await interaction.response.send_message("Ta commande a bien été enregistré")
     identifiant += 1
 
-@bot.slash_command(name="valider", description="pour valider une commande", options=[
-    Option(name="nombre", description="le numéro de commande à valider", option_type=4, required=True)])
-async def valider(ctx:APPCI, nombre):
+@tree.command(name="valider")
+@app_commands.describe(nombre="le numéro de la commande que tu veux valider")
+async def valider(interaction: discord.Interaction, nombre:int):
     '''
     to validate a command
     '''
-    channel = bot.get_channel(1094212525157007410)
-    messages = await channel.history(limit=200).flatten()
-    for message in messages:
-        if message.position == nombre:
+    with open("commands_channel.txt", "r") as commands_channel:
+        channel = client.get_channel(int(commands_channel.read()))
+    real_messages = []
+    messages = channel.history(limit=None)
+    async for message in messages:
+        real_messages.append(message)
+    for i in range(len(real_messages)):
+        if i == nombre-1:
             (_, people, title) = commandes[nombre]
             now = datetime.now().strftime("%H:%M")
-            new_emb = discord.Embed(title=title, description=f"Commande n°{nombre} validé par {ctx.author.mention} à {now}", color=0x51ff00)
-            await message.edit(embed=new_emb)
+            new_emb = discord.Embed(title=title, description=f"Commande n°{nombre} validé par {interaction.user.mention} à {now}", color=0x51ff00)
+            await real_messages[i].edit(embed=new_emb)
             await people.send(f"Ta commande n°{nombre} a bien été validé et est en préparation.\nNous te renverrons un message lorsque ta commande sera prête")
+            await interaction.response.send_message(f"La commande n°{nombre} a bien été validé")
+        else:
+            await interaction.response.send_message("Je n'ai pas la trouvé de commande portant ce numéro")
 
 with open('token_bot.txt', 'r') as token:
-    bot.run(token.read())
+    client.run(token.read())
